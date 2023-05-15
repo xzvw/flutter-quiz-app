353b0a51e1bd4de192dc887ad0a1d47c5ec09093
xzvw <128382145+xzvw@users.noreply.github.com>
2023-05-15 18:43:53 +0800

    Add `AnswerButton` widget

---
+++ lib/answer_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  final String text;
+  final void Function() onPressed;
+
+  const AnswerButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 32,
+          vertical: 8,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            32,
+          ),
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
