d1b4eb44eae4135644745e18ae8e1a3e41c009a5
xzvw <128382145+xzvw@users.noreply.github.com>
2023-05-21 00:58:19 +0800

    Add `SingleChildScrollView` to `Summary` widget and update styles

--- lib/summary.dart
+++ lib/summary.dart
@@ -7,28 +7,39 @@ class Summary extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Column(
-      children: summary
-          .map(
-            (entry) => Row(
-              children: [
-                Text(
-                  ((entry['index'] as int) + 1).toString(),
-                ),
-                Column(
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summary
+              .map(
+                (entry) => Row(
                   children: [
-                    Text(entry['question'] as String),
-                    const SizedBox(
-                      height: 16,
+                    Text(
+                      ((entry['index'] as int) + 1).toString(),
                     ),
-                    Text(entry['correct_answer'] as String),
-                    Text(entry['selected_answer'] as String),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          entry['question'] as String,
+                          style: const TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Text(entry['correct_answer'] as String),
+                        Text(entry['selected_answer'] as String),
+                      ],
+                    )
                   ],
-                )
-              ],
-            ),
-          )
-          .toList(),
+                ),
+              )
+              .toList(),
+        ),
+      ),
     );
   }
 }
